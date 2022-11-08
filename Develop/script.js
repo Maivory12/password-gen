8edff56a267b9eb2ef905719a1b2ef5b74f97d6e
Marques Ivory <Maivory12@icloud.com>
2022-11-08 12:08:39 -0500

    Added prompt to choose a password length between 8 and 128 characters

--- Develop/script.js
+++ Develop/script.js
@@ -3,6 +3,13 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  var passwordLength = (prompt("choose password length between 8 and 128 characters"))
+  console.log(passwordLength)
+  if (!passwordLength) {
+    alert("You must choose a number");
+  } else if (passwordLength<8 || passwordLength>128) {
+    alert("You must choose between 8 and 128 characters")
+  }
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -11,4 +18,5 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword) 
+
