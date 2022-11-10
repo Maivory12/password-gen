bce19092685ec1b083cce1883d68e6ac45469430
Marques Ivory <Maivory12@icloud.com>
2022-11-09 21:11:56 -0500

    added comments to script.js

--- Develop/script.js
+++ Develop/script.js
@@ -12,34 +12,42 @@ var password = ``
 function writePassword() {
   var passwordLength = parseInt(prompt("choose password length between 8 and 128 characters"))
   console.log(passwordLength)
-  //
+  //Alerts user that a number must be entered.
   if (!passwordLength) {
 
     alert("You must choose a number");
 
-  } else if (passwordLength<8 || passwordLength>128)
+  } 
+  //Alerts user to choose a password length between 8 and 128 characters.
+
+  else if (passwordLength<8 || passwordLength>128)
   {
     alert("You must choose between 8 and 128 characters")
   }
   else {
-    //
+  //Allows user to choose uppercase characters to add to password.
+
     if (confirm(`Do you want an uppercase character?`)) {
       typeOfcharacter +=  upperCase;
     }
-    //
+  //Allows user to choose lowercase characters to add to password.
+
     if (confirm(`Do you want an lowercase character?`)) {
       typeOfcharacter += lowerCase;
     }
-    //
+  //Allows user to choose numbers to add to password.
+
     if (confirm(`Do you want a number character?`)) {
       typeOfcharacter += number;
 
     } 
-    //
+   // Allows user to choose symbols to add to password.
+
     if (confirm(`Do you want a symbol?`)) {
       typeOfcharacter += symbol;
     }
-    //
+  // For loop to choose random items from list of choices if selected.
+
     if (typeOfcharacter.length >= 1){
       for (var i = 0; i <= passwordLength; i++){
 
@@ -47,7 +55,8 @@ function writePassword() {
       }
     
     }
-    // 
+    // User is alerted if no criteria is met.
+    
       else {
       alert(`You must choose a character, number, or symbol.`);
     
